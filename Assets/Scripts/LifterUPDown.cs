8251f9d9380f472f590ae771f48665bdbae58ced
IvoIvanovskii <140730272+IvoIvanovskii@users.noreply.github.com>
2023-08-21 15:23:12 +0200

    forklift controlls

--- Assets/Scripts/LifterUPDown.cs
+++ Assets/Scripts/LifterUPDown.cs
@@ -1,20 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class LifterUPDown : MonoBehaviour
 {
-    Transform lifterUp;
-    Transform lifterDown;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] Transform liftTransfrom;
+
+
+    [SerializeField] Vector2 minLiftPosition;
+    [SerializeField] Vector2 maxLiftPosition;
+
+
+    //shows the speed of the lifter with and without the palett
+    [SerializeField] float liftWithoutPalett;
+    [SerializeField] float liftWithPalett;
+    
+
+    
+    Vector2 joystickValue;
+
+    void UpdateLiftPosition(float speed){
+        var liftPosition = liftTransfrom.localPosition;
+
+        liftPosition += new Vector3(joystickValue.x * speed * Time.deltaTime, 0f, joystickValue.y * speed * Time.deltaTime);            
+
+        liftPosition.x = Mathf.Clamp(liftPosition.x, minLiftPosition.x, maxLiftPosition.x);
+        liftPosition.y = Mathf.Clamp(liftPosition.y, minLiftPosition.y, maxLiftPosition.y);
+
+
+        liftTransfrom.localPosition = liftPosition;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+public void OnJoystickValueChangeX(float x){
+        joystickValue.x = x;
+    }
+    public void OnJoystickValueChangeY(float y){
+        joystickValue.y = y;
     }
 }
