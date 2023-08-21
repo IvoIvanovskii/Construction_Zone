2103e958306f19c49b8192ba5474553bc05d1dbc
IvoIvanovskii <140730272+IvoIvanovskii@users.noreply.github.com>
2023-08-21 15:56:35 +0200

    changing scripts for controlls

--- Assets/Scripts/LifterUPDown.cs
+++ Assets/Scripts/LifterUPDown.cs
@@ -1,41 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class LifterUPDown : MonoBehaviour
 {
     [SerializeField] Transform liftTransfrom;
 
-
-    [SerializeField] Vector2 minLiftPosition;
-    [SerializeField] Vector2 maxLiftPosition;
-
-
-    //shows the speed of the lifter with and without the palett
-    [SerializeField] float liftWithoutPalett;
-    [SerializeField] float liftWithPalett;
+    public void OnJoystickValueChangeX(float x) => liftTransfrom.localPosition =  new Vector3( Mathf.Lerp(0, 1.7f, x), liftTransfrom.localPosition.y, liftTransfrom.localPosition.z);
+    public void OnJoystickValueChangeY(float y) => liftTransfrom.localPosition =  new Vector3(liftTransfrom.localPosition.x, Mathf.Lerp(0, 1.7f, y), liftTransfrom.localPosition.z);
     
-
-    
-    Vector2 joystickValue;
-
-    void UpdateLiftPosition(float speed){
-        var liftPosition = liftTransfrom.localPosition;
-
-        liftPosition += new Vector3(joystickValue.x * speed * Time.deltaTime, 0f, joystickValue.y * speed * Time.deltaTime);            
-
-        liftPosition.x = Mathf.Clamp(liftPosition.x, minLiftPosition.x, maxLiftPosition.x);
-        liftPosition.y = Mathf.Clamp(liftPosition.y, minLiftPosition.y, maxLiftPosition.y);
-
-
-        liftTransfrom.localPosition = liftPosition;
-    }
-
-public void OnJoystickValueChangeX(float x){
-        joystickValue.x = x;
-    }
-    public void OnJoystickValueChangeY(float y){
-        joystickValue.y = y;
-    }
 }
