c5cd5cfce7616c341e591274628725a97838bece
IvoIvanovskii <140730272+IvoIvanovskii@users.noreply.github.com>
2023-08-29 16:54:04 +0200

    wheel colliders/ need fixing

--- Assets/Scripts/ForkliftController.cs
+++ Assets/Scripts/ForkliftController.cs
@@ -1,28 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-[RequireComponent(typeof(Rigidbody))]
 public class ForkliftController : MonoBehaviour
 {
-    [Header("Movement Options")]
-    [SerializeField] float movementSpeed;
-    [SerializeField] Vector3 rotationAxis;
-    [SerializeField] float movementStick;
+    public ActionBasedController leftController;
+    public ActionBasedController rightController;
 
 
-    float currSpeed;
-    private Rigidbody rb;
+    public Transform offset;
+    public Transform steeringWheel;
+    public Transform steeringWheelChild;
+    public Transform maxSteerAngle;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public WheelCollider FLwheel;
+    public WheelCollider FRwheel;
+    public WheelCollider BLwheel;
+    public WheelCollider BRwheel;
+
+    
+    public Transform BLwheelTransform;
+    public Transform BRwheelTransform;
+    public float accelerationForce;
+    public float breakForce;
+
+
+    private Transform target;
+    private Vector3 fromCector;
+    private bool steered;
+    private float angleBetween;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player"){
+            target = other.transform;
+
+            offset.position = target.position;
+            offset.localPosition = new Vector3(offset.localPosition.x, 0, offset.localPosition.z);
+            Vector3 dir = offset.position - transform.position;
+            Quaternion rot = Quaternion.LookRotation(dir, transform.up);
+
+            steeringWheelChild.SetParent(null);
+            steeringWheel.rotation = rot;
+            steeringWheelChild.SetParent(steeringWheel);
+        }
+    }
+    
+    private void  OnTriggerStay(Collider other) {
+        if(other.tag == "Player"){
+            target = other.transform;
+        }
     }
 }
