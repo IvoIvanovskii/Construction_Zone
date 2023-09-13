329e2051ae7a77ed0fa552c7e9cd623654bbb04d
IvoIvanovskii <140730272+IvoIvanovskii@users.noreply.github.com>
2023-09-13 16:10:26 +0200

    ui fix

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.InputSystem; 
 
 public class GameMenuManager : MonoBehaviour
 {
     public Transform head;
     public float spawnDistance = 2f;
     public GameObject menu;
-    public InputActionProperty showButton;
+    bool hasPressedButton = false;
 
    private void Update(){
-    
-    if(showButton.action.WasPressedThisFrame()){
-        menu.SetActive(!menu.activeSelf);
-
-        menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+    var device = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, device);
         
+    if (device[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.menuButton, out bool pressed) && pressed)
+    if(pressed && !hasPressedButton){
+        Debug.LogError("menu appear");
+        menu.SetActive(!menu.activeSelf);
+        menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+        hasPressedButton = true;
+    }else{
+        hasPressedButton = false;
     }
    
    }
