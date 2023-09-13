a65c532ef3da3ea2ba068f737e45565b8dec1a73
IvoIvanovskii <140730272+IvoIvanovskii@users.noreply.github.com>
2023-09-13 13:44:29 +0200

    .

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class GameMenyManager : MonoBehaviour
+public class GameMenuManager : MonoBehaviour
 {
     public Transform head;
     public float spawnDistance = 2f;
     public GameObject menu;
     public InputActionProperty showButton;
 
-   private void Update() {
+   private void Update(){
+    
     if(showButton.action.WasPressedThisFrame()){
-        menu.SetActive(menu.activeSelf);
+        menu.SetActive(!menu.activeSelf);
 
         menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+        
     }
-        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
-        menu.transform.forward *= -1;
+   
    }
 
 }
